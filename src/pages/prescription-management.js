b9062b80cd2eb3bf8c542a4c4f9cbde6919c0f8b
JOY <joybarua0058@gmail.com>
2023-08-22 13:46:38 +0600

    working on prescription management

--- src/pages/prescription-management.js
+++ src/pages/prescription-management.js
@@ -1,25 +1,25 @@
 import RootLayout from "@/components/Layouts/RootLayout";
 import { useState } from "react";
 
-const PrescriptionManagementPage = () => {
+const PrescriptionManagementPage = ({ patients }) => {
   const [selectedPatient, setSelectedPatient] = useState(null);
   const [prescription, setPrescription] = useState("");
 
-  const patients = [
-    {
-      id: 1,
-      name: "Patient 1",
-      age: 30,
-      phoneNumber: "1234567890",
-      address: "Address 1",
-      previousIssues: ["Anxiety", "Insomnia"],
-      currentIssues: "Depression",
-      sleepMode: "Trouble falling asleep",
-      depression: ["Loss of interest", "Low energy"],
-      ocd: ["Compulsive behavior"],
-    },
-    // Add more patients
-  ];
+  // const patients = [
+  //   {
+  //     id: 1,
+  //     name: "Patient 1",
+  //     age: 30,
+  //     phoneNumber: "1234567890",
+  //     address: "Address 1",
+  //     previousIssues: ["Anxiety", "Insomnia"],
+  //     currentIssues: "Depression",
+  //     sleepMode: "Trouble falling asleep",
+  //     depression: ["Loss of interest", "Low energy"],
+  //     ocd: ["Compulsive behavior"],
+  //   },
+  //   // Add more patients
+  // ];
 
   const handlePatientChange = (e) => {
     const patientId = parseInt(e.target.value);
@@ -65,12 +65,12 @@ const PrescriptionManagementPage = () => {
               <p>Phone Number: {selectedPatient.phoneNumber}</p>
               <p>Address: {selectedPatient.address}</p>
               <p>
-                Previous Issues: {selectedPatient.previousIssues.join(", ")}
+                Previous Issues: {selectedPatient?.previousIssues?.join(", ")}
               </p>
-              <p>Current Issues: {selectedPatient.currentIssues}</p>
-              <p>Sleep Mode: {selectedPatient.sleepMode}</p>
-              <p>Depression: {selectedPatient.depression.join(", ")}</p>
-              <p>OCD: {selectedPatient.ocd.join(", ")}</p>
+              <p>Current Issues: {selectedPatient?.currentIssues}</p>
+              <p>Sleep Mode: {selectedPatient?.sleepMode}</p>
+              <p>Depression: {selectedPatient?.depression?.join(", ")}</p>
+              <p>OCD: {selectedPatient?.ocd?.join(", ")}</p>
             </div>
             <label className="block mb-4">
               Write Prescription:
@@ -99,4 +99,17 @@ export default PrescriptionManagementPage;
 
 PrescriptionManagementPage.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
+};
+
+export const getServerSideProps = async () => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/v1/patient`
+  );
+  const data = await res.json();
+
+  return {
+    props: {
+      patients: data.data,
+    },
+  };
 };
